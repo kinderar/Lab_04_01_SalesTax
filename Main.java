eb868c8a4465a18f6c45f48e076d4ebbb9af9878
kinderar <156221770+kinderar@users.noreply.github.com>
2024-02-04 16:28:02 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,8 +7,8 @@ public class Main {
         double computedSalesTax = 0;
         int itemPrice = 70; //input for now
         double totalPrice = 0; //Price including sales tax
-            computedSalesTax = (int) (salesTax * itemPrice);
-            totalPrice = (int) (itemPrice + computedSalesTax);
+            computedSalesTax = (int) (salesTax * itemPrice); //finding sales tax
+            totalPrice = (int) (itemPrice + computedSalesTax); //putting total price together
         System.out.println("The item price of " + itemPrice + " plus the computed sales tax of " + computedSalesTax + " comes to a total of " +totalPrice);
     }
 }
